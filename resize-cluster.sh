e7d91cfc9cd48ae4296cb150f3337800a4c9aecd
刘成 <790809568@qq.com>
2019-06-28 17:41:50 +0800

    更改配置

--- resize-cluster.sh
+++ resize-cluster.sh
@@ -11,18 +11,20 @@ fi
 
 # change slaves file
 i=1
-rm docker-config/hadoop/workers
-rm docker-config/spark/slaves
-rm docker-config/zookeeper/zoo.cfg
+echo > docker-config/hadoop/workers
+echo >  docker-config/spark/slaves
+echo >  docker-config/zookeeper/zoo.cfg
+echo "spark-master" >> docker-config/hadoop/workers
 echo "dataDir=/usr/local/zookeeper/apache-zookeeper-3.5.5/tmp" >> docker-config/zookeeper/zoo.cfg
+echo "server.1 = spark-master:2888:3888" >> docker-config/zookeeper/zoo.cfg
 while [[ ${i} -lt ${N} ]]
 do
 	echo "spark-slave$i" >> docker-config/hadoop/workers
-	echo "spark-slave$i" >> docker-config/spark/slaves
-	echo "server.$i=spark-slave$i:2888:3888" >> docker-config/zookeeper/zoo.cfg
+	j=i+1
+	echo "server.$j=spark-slave$i:2888:3888" >> docker-config/zookeeper/zoo.cfg
 	((i++))
 done 
-
+mv -f docker-config/hadoop/workers docker-config/spark/slaves
 echo ""
 
 echo -e "\nbuild docker spark image\n"
